e928ed6b61af72a84fcf1aa7f05864cefa4450d0
Furqan <furqanpersonal@gmail.com>
2020-05-28 16:05:03 +0500

    Initial Commit

---
+++ Todoey/TodoListTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TodoListTableViewController.swift
+//  Todoey
+//
+//  Created by Furqan on 28/05/2020.
+//  Copyright © 2020 Furqan. All rights reserved.
+//
+
+import UIKit
+
+class TodoListTableViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggs", "Destory Car"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //MARK: TableView Delegate Method
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+//    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+
+   
+
+}
